6468558971ba387f40e621fb9ec6123c9da064ac
Rafael <pereira.fael.pereira@gmail.com>
2023-09-11 12:27:37 -0300

    Tipos e variaveis

---
+++ Meu projeto/src/edu/rafael/TiposVariaveis.java
@@ -0,0 +1,14 @@
+package edu.rafael;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salariominimo = 2500;
+
+        short numerocurto = 1;
+        int numeronormal = numerocurto;
+        short numerocurto2 = (short)numeronormal;
+
+        final double PI = 3.14;
+
+    }
+}
