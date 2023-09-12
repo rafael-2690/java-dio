ea9a3843ec35df8fc30bb2d7d13feb0a0e0d0104
Rafael <pereira.fael.pereira@gmail.com>
2023-09-12 09:12:41 -0300

    projeto smartTv

---
+++ Meu projeto/src/edu/rafael/projeto/Usuario.java
@@ -0,0 +1,19 @@
+package edu.rafael.projeto;
+
+public class Usuario {
+    public static void main(String[] args) {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.ligar();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        smartTv.mudarCanal(267);
+
+        System.out.println("TV ligada: " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("VOlume atual: " + smartTv.volume);
+
+}
+}
